b1138f1829ff7faa3479d4fcd3762b6157e293c7
Bryan Chiew <bryanchiew777@gmail.com>
2024-06-24 14:26:31 +0800

    3rd commit

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: nextConfig = {
+        ignoreDuringBuilds: true,
+    },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
+//export default nextConfig;
